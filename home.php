4886c94ab3fcc2840675bebc6cd48389e6697c3c
Michele Pellegrino <michele.pellegrino.133@itiscuneo.eu>
2025-04-23 08:31:23 +0200

    home

---
+++ home.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+
+include("./templates/areaRiservata/headerRiservata.php");
+include("./templates/areaRiservata/navbar.php");
+?>
---
+++ templates/AreaRiservata/navbar.php
@@ -0,0 +1,4 @@
+<nav>
+    <a href="./inserisciDatiVendita.php"><button>Inserisci dati vendita</button></a>
+    <a href="./visualizzaDatiVisita.php"><button>Registro vendita</button></a>
+</nav>
